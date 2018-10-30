565f9605a2da49e50b805fbcf02b3ddf67637718
Tamas Hrabovszki <hunhrabo@gmail.com>
2018-10-30 23:25:20 +0200

    Minified html and css files

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify-es').default,
+    minifyCSS = require('gulp-clean-css'),
+    minifyHTML = require('gulp-htmlmin'),
     concat = require('gulp-concat');
 
 var env,
@@ -63,6 +65,7 @@ gulp.task('compass', function() {
         style: sassStyle
     })
         .on('error', gutil.log))
+    .pipe(minifyCSS())
     .pipe(gulp.dest(outputDir + 'css'))
     .pipe(connect.reload())
 });
@@ -71,7 +74,7 @@ gulp.task('watch', function() {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*scss', ['compass']);
-    gulp.watch(htmlSources, ['html']);
+    gulp.watch('builds/development/*.html', ['html']);
     gulp.watch(jsonSources, ['json']);
 });
 
@@ -83,7 +86,9 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-    gulp.src(htmlSources)
+    gulp.src('builds/development/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML({ collapseWhitespace: true })))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
     .pipe(connect.reload());
 });
 
